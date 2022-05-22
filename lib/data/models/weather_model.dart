4db45bab4ab35088ad45fc94b2c7ac779fc46ef5
reinans <reinandesouza01@gmail.com>
2022-05-22 14:18:17 -0300

    ref: change weather api model

--- lib/data/models/weather_model.dart
+++ lib/data/models/weather_model.dart
@@ -1,60 +1,34 @@
-import 'dart:convert';
-
 import 'package:equatable/equatable.dart';
+import 'package:weather_app/data/models/current_model.dart';
+import 'package:weather_app/data/models/forecast_model.dart';
+import 'package:weather_app/data/models/location_model.dart';
 import 'package:weather_app/domain/entities/weather.dart';
+
+//     final weatherModel = weatherModelFromJson(jsonString);
 
 class WeatherModel extends Equatable {
   const WeatherModel({
-    required this.cityName,
-    required this.main,
-    required this.description,
-    required this.iconCode,
-    required this.temperature,
-    required this.pressure,
-    required this.humidity,
+    required this.location,
+    required this.current,
+    required this.forecast,
   });
 
-  final String cityName;
-  final String main;
-  final String description;
-  final String iconCode;
-  final double temperature;
-  final int pressure;
-  final int humidity;
+  final LocationModel location;
+  final CurrentModel current;
+  final ForecastModel forecast;
 
-  factory WeatherModel.fromMap(Map<String, dynamic> map) {
-    return WeatherModel(
-      cityName: map['name'] ?? '',
-      main: map['weather'][0]['main'] ?? '',
-      description: map['weather'][0]['description'] ?? '',
-      iconCode: map['weather'][0]['icon'] ?? '',
-      temperature: map['main']['temp']?.toDouble() ?? 0.0,
-      pressure: map['main']['pressure']?.toInt() ?? 0,
-      humidity: map['main']['humidity']?.toInt() ?? 0,
-    );
-  }
-
-  factory WeatherModel.fromJson(Map<String, dynamic> source) =>
-      WeatherModel.fromMap(source);
+  factory WeatherModel.fromJson(Map<String, dynamic> json) => WeatherModel(
+        location: LocationModel.fromJson(json["location"]),
+        current: CurrentModel.fromJson(json["current"]),
+        forecast: ForecastModel.fromJson(json["forecast"]),
+      );
 
   Weather toEntity() => Weather(
-        cityName: cityName,
-        main: main,
-        description: description,
-        iconCode: iconCode,
-        temperature: temperature,
-        pressure: pressure,
-        humidity: humidity,
+        location: location.toEntity(),
+        current: current.toEntity(),
+        forecast: forecast.toEntity(),
       );
 
   @override
-  List<Object?> get props => [
-        cityName,
-        main,
-        description,
-        iconCode,
-        temperature,
-        pressure,
-        humidity,
-      ];
+  List<Object?> get props => [location, current, forecast];
 }
